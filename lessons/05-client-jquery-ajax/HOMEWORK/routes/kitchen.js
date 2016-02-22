c29b70ccf00dc8fa09b2a8e3255f0b1e3896b7a1
Sarah Walters <sarah.walters@students.olin.edu>
2016-02-22 18:59:49 -0500

    Homework 5 feedback

--- lessons/05-client-jquery-ajax/HOMEWORK/routes/kitchen.js
+++ lessons/05-client-jquery-ajax/HOMEWORK/routes/kitchen.js
@@ -1,4 +1,4 @@
-var db = require('../db');
+var db = require('../db'); // requiring this connects to the database, yes? You should only need to connect once -- ie only require it in app.js
 var Ingredient = require('../models/ingredientModel');
 var Order = require('../models/orderModel');
 
@@ -10,11 +10,14 @@ routes.home = function(req, res){
         .exec(function(err, orders){
             if(err) console.error(err);
 
-            // Aw yeh, functional programming
-            priced = orders.map(function(order){
+            // Aw yeh, functional programming :)
+            var priced = orders.map(function(order){
 
                 // Check if an ingredient was deleted which is needed
                 var nulled = order.ingredients.filter(function(ingredient){ return ingredient.ingredient == undefined });
+
+                // Not clear to me what case you're catching here -- so nulled is the list of ingredients which are undefined,
+                // and if *that's* not truthy you raise an error? Why?
                 if( !nulled ){
                     // Could have better error handling
                     console.error("underfined ingredient!");
@@ -24,8 +27,8 @@ routes.home = function(req, res){
 
                 var price = order.ingredients.reduce(function(prev, cur, idx, arr){
                     return prev + cur.ingredient.price*cur.amount;
-                }, 0);
-                
+                }, 0); // nice!
+
                 order.price = "" + price.toFixed(2); // Force it to be pretty
                 return order;
             });
