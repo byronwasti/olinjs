3ec98dab229268e17d346707fb8bfccb9feac355
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2016-04-09 14:29:15 -0400

    ADD: keys env based ids file

---
+++ lessons/06-css-development-style/Twoter/keys.js
@@ -0,0 +1,7 @@
+var ids = {
+    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+    FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
+    FACEBOOK_CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL
+}
+
+module.exports = ids;
