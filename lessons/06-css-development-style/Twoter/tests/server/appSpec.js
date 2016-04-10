5cedfbd0fa6f5330d516a405845f0dacb471c4ac
byronwasti <byron.wasti@gmail.com>
2016-04-10 15:07:17 -0400

    More tests added for Twoter

--- lessons/06-css-development-style/Twoter/tests/server/appSpec.js
+++ lessons/06-css-development-style/Twoter/tests/server/appSpec.js
@@ -14,9 +14,35 @@ describe("The app", function(){
             });
     });
 
+    it('should return 200 OK on GET /byUser', function(done){
+        request(app)
+            .get('/byUser')
+            .expect(200)
+            .end(function(err, res){
+                if(err){
+                    return done(err);
+                }
+                done();
+            });
+    });
+
+    it('should return HTML on GET /', function(done){
+        request(app)
+            .get('/')
+            .expect('Content-Type', 'text/html; charset=utf-8')
+    });
+
+    it('should return 404 on GET /notaroute', function(done) {
+        request(app)
+            .get('/notaroute')
+            .expect(404, done);
+    });
+
     it('should return 401 no Authorized on POST /userPost', function(done){
-        // TODO: Add a test for this
-        done();
+        request(app)
+            .post('/twote')
+            .send({ text: "test" })
+            .expect(401, done);
     });
 });
 
